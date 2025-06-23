bc8cb25628388bad7ca7cde02f9ee554e3414f96
Jeevika-05 <jeevikajkkn@gmail.com>
2025-06-23 13:32:24 +0530

    Update UDP server.py

--- UDP server.py
+++ UDP server.py
@@ -2,7 +2,7 @@ import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("0.0.0.0", 9999))
-print("✅ UDP Server running on port 9999...")
+print(" UDP Server running on port 9999...")
 
 while True:
     data, addr = sock.recvfrom(1024)
